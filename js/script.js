469895b691b77088eb256c5ed1b6d10bb2da6898
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-29 01:46:23 +0200

    sistemare riga 25

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 //stampiamo in console 5 numeri ccasuali
     
-let numeriGenerati = []
+let numeriGenerati = []     //creiamo una variabile in cui conservare l'array di numeri casuali generati dalla funzione
+let numeriUtente = []      //creiamo una variabile in cui conserare un array dei numeri scritti dall'utente nel prompt
+console.log(numeriUtente)
 
-function numeriCasuali() {
+function numeriCasuali() {     // creiamo una funzione in grado di generare numeri casuali da 1 a 100
     return Math.floor(Math.random() * 100 +1);
   }
 
 
-  for (let i = 0; i < 5; i++) {
-    let numeroCasuale;
+  for (let i = 0; i < 5; i++) {   // cicliamo sulla funzione precedente per poter creare almeno 5 numeri da 1 a 100
+    let numeroCasuale;            
     numeroCasuale = numeriCasuali();
-    numeriGenerati.push(numeroCasuale)
+    numeriGenerati.push(numeroCasuale) // li pushamo nella variabile in alto per formare un array di 5 numeri
     
 }
-alert("memorizza questi numeri in pagina")
+alert("memorizza questi numeri in pagina")   // a questo punto chiediamo all'utente di memorizzare i dati presenti in pagina...verra eseguita al caricamento della pagina
 document.getElementById("numeri_casuali").innerText = numeriGenerati
-console.log(numeriGenerati.length)
 
-setTimeout(fineTempo, 2 * 1000);
 
-function fineTempo (){
-    let domanda;
-    do
-    domanda = prompt("inserisci i numeri a schermo") 
-    while (domanda <= numeriGenerati.lenght);
+setTimeout(fineTempo, 3 * 1000); // creiamo una funzione tempo in cui settiamo un timing di 30 sec
 
-    
+function fineTempo (){  // alll'interno di questa funzione stabiliamo quello che avverrà dopo i 30 secondi
+   // let elemento = document.getElementsByClassName("nascondi")
+    // console.log(elemento)
+    for (let i = 0; i < numeriGenerati.length; i++) {  // cicliamo sui numeri generati prima per poterli estrapolare singolarmente
+        let domanda = prompt("Ricordi qualche numero?")  // facciamo continuare a chiedere il prompt per 5 volte ovvelo la lunghezza dell'array di numeri generati
+        if(domanda==numeriGenerati[i]){  // confrontiamo i due valori,
+            numeriUtente.push(domanda)  // se il numero scritto dall'utente nel prompt e uguale a uno dei valori in numeri generati, lo pushamo nell'array sopra
+        }
+    }
+    alert("hai indovinato i seguenti numeri " + numeriUtente)  // avvisiamo l'utente di quali numeri ha indovinato
 }
-
